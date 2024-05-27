e4c1dd864107a1e513a3861f1b7087bd0fe1791a
Daniel Daneshi <danieldotinc@gmail.com>
2024-05-27 23:04:49 +0200

    adjust styling and naming

--- app/page.tsx
+++ app/page.tsx
@@ -34,13 +34,13 @@ export default function Home() {
 
   return (
     <div className="group w-full overflow-auto pl-0 peer-[[data-state=open]]:lg:pl-[250px] peer-[[data-state=open]]:xl:pl-[300px]">
-      <main className="fixed inset-x-0 w-full bg-gradient-to-b from-muted/30 from-0% to-muted/30 to-50% duration-300 ease-in-out animate-in dark:from-background/10 dark:from-10% dark:to-background/80 peer-[[data-state=open]]:group-[]:lg:pl-[250px] peer-[[data-state=open]]:group-[]:xl:pl-[300px]">
+      <main className="fixed inset-x-0 w-full peer-[[data-state=open]]:group-[]:lg:pl-[250px] peer-[[data-state=open]]:group-[]:xl:pl-[300px]">
         <div className="mx-auto sm:max-w-4xl sm:px-4">
           <div className="mb-4 grid grid-cols-2 gap-2 px-4 sm:px-0">
             {exampleMessages.map((example, index) => (
               <div
                 key={example.heading}
-                className={`cursor-pointer rounded-lg border bg-white p-4 hover:bg-zinc-50 dark:bg-zinc-950 dark:hover:bg-zinc-900 ${
+                className={`cursor-pointer rounded-lg border p-4 hover:bg-zinc-800 dark:bg-zinc-950 dark:hover:bg-zinc-900 ${
                   index > 1 && 'hidden md:block'
                 }`}
                 onClick={() => !!example.link && router.push(example.link)}
