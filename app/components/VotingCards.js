50a060a91e8074bba7ae5d48f9a249ec9e3070b4
Daniel Daneshi <danieldotinc@gmail.com>
2024-05-27 09:57:08 +0200

    add animation for card pick

--- app/components/VotingCards.js
+++ app/components/VotingCards.js
@@ -1,10 +1,5 @@
-'use client';
-
-import { useState } from 'react';
-
 const VotingCards = ({ onVote, story, username }) => {
   const cards = [0.5, 1, 2, 3, 5, 8, 'Pass'];
-  const [selected, setSelected] = useState(story.votes[username]);
 
   return (
     <div className="flex justify-center space-x-4">
@@ -12,9 +7,9 @@ const VotingCards = ({ onVote, story, username }) => {
         <button
           key={card}
           onClick={() => onVote(card)}
-          className={`p-11  hover:bg-amber-500 rounded text-2xl ${
-            story.votes[username] == card ? 'bg-red-500' : 'bg-primary'
-          }`}
+          className={`w-16 py-8 hover:transition hover:transform hover:-translate-y-6 ${
+            story.votes[username] == card ? '-translate-y-6' : ''
+          } hover:bg-amber-500 rounded text-xl ${story.votes[username] == card ? 'bg-red-500' : 'bg-primary'}`}
         >
           {card}
         </button>
