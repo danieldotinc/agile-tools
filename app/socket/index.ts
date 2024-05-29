21aa7c1cbcd1f98fbbc849fb39342be10a2b5ed2
Daniel Daneshi <daniel.daneshi@censhare.com>
2024-05-29 14:18:10 +0200

    adjust link

--- app/socket/index.ts
+++ app/socket/index.ts
@@ -1,6 +1,6 @@
 import io from 'socket.io-client';
 
-// const socket = io('https://www.jokerway.de/');
-const socket = io('http://localhost:3000');
+const socket = io('https://www.jokerway.de/');
+// const socket = io('http://localhost:3000');
 
 export default socket;
