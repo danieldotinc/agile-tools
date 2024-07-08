665c95ed97ce09f9e0e91b7b63f576f945aa71f1
Daniel Daneshi <daniel.daneshi@censhare.com>
2024-07-08 16:47:32 +0200

    feature: add card / number toggle button

---
+++ app/components/UserCardType.tsx
@@ -0,0 +1,36 @@
+'use client';
+import React, { useState } from 'react';
+
+const UserCardType = ({ onCardTypeChange }: { onCardTypeChange: (cardType: string) => void }) => {
+  const [cardTypeName, setCardType] = useState(localStorage.getItem('card') ?? 'card');
+  const isCard = cardTypeName === 'card';
+
+  const handleToggle = () => {
+    const newCardType = isCard ? 'number' : 'card';
+    onCardTypeChange(newCardType);
+    localStorage.setItem('card', newCardType);
+    setCardType(newCardType);
+  };
+
+  return (
+    <div className="flex items-center p-4">
+      <label className="inline-flex items-center cursor-pointer">
+        <input type="checkbox" className="hidden" checked={isCard} onChange={handleToggle} />
+        <span
+          className={`w-10 h-6 flex items-center  rounded-full p-1 duration-300 ease-in-out ${
+            isCard ? 'bg-prominent' : 'bg-gray-300'
+          }`}
+        >
+          <span
+            className={`bg-white w-4 h-4 rounded-full shadow-md transform duration-300 ease-in-out ${
+              isCard ? 'translate-x-4 bg-black' : ''
+            }`}
+          ></span>
+        </span>
+        <span className="ml-2 text-gray-400">cards</span>
+      </label>
+    </div>
+  );
+};
+
+export default UserCardType;
--- app/components/UserCards.js
+++ app/components/UserCards.js
@@ -1,6 +1,10 @@
+'use client';
 import Image from 'next/image';
+import UserCardType from './UserCardType';
+import React, { useState } from 'react';
 
 const UserCards = ({ users, stories, currentIndex }) => {
+  const [cardType, setCardType] = useState(localStorage.getItem('card') ?? 'card');
   const revealed = stories[currentIndex].revealed;
 
   const getCard = (vote) => {
@@ -9,30 +13,48 @@ const UserCards = ({ users, stories, currentIndex }) => {
     return 'back';
   };
 
+  const handleCardTypeChange = (ct) => setCardType(ct);
+
   return (
-    <div className="flex justify-center space-x-4 mb-4 flex-wrap">
-      {revealed
-        ? Object.entries(stories[currentIndex].votes).map(([username, vote]) => (
-            <div key={username} className={`p-2 text-dark`}>
-              <div className="text-center font-normal">{username}</div>
-              <Image src={`/assets/${getCard(vote)}.png`} className="w-24 h-32" width={175} height={300} alt="vote" />
-            </div>
-          ))
-        : users
-            .filter((u) => u.username !== 'admin')
-            .map((user) => (
-              <div key={user.id} className={`p-2 text-dark`}>
-                <div className="text-center font-normal">{user.username}</div>
-                <Image
-                  src={`/assets/${getCard(stories[currentIndex].votes[user.username])}.png`}
-                  className="w-24 h-32"
-                  width={175}
-                  height={300}
-                  alt="vote"
-                />
+    <>
+      <UserCardType onCardTypeChange={handleCardTypeChange} />
+      <div className="flex justify-center space-x-4 mb-4 flex-wrap">
+        {revealed
+          ? Object.entries(stories[currentIndex].votes).map(([username, vote]) => (
+              <div key={username} className={`p-2 text-dark`}>
+                <div className="text-center font-normal">{username}</div>
+                {cardType === 'card' ? (
+                  <Image
+                    src={`/assets/${getCard(vote)}.png`}
+                    className="w-24 h-32"
+                    width={175}
+                    height={300}
+                    alt="vote"
+                  />
+                ) : (
+                  <div className="w-24 h-32 rounded text-5xl bg-gray-100 text-black flex justify-center items-center">
+                    {vote}
+                  </div>
+                )}
               </div>
-            ))}
-    </div>
+            ))
+          : users
+              .filter((u) => u.username !== 'admin')
+              .map((user) => (
+                <div key={user.id} className={`p-2 text-dark`}>
+                  <div className="text-center font-normal">{user.username}</div>
+
+                  <Image
+                    src={`/assets/${getCard(stories[currentIndex].votes[user.username])}.png`}
+                    className="w-24 h-32"
+                    width={175}
+                    height={300}
+                    alt="vote"
+                  />
+                </div>
+              ))}
+      </div>
+    </>
   );
 };
 
